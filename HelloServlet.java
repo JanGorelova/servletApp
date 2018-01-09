eadcd3cd3e9d87967e2fb1ed494f7077d0470c23
Zhanna_Gorelova <Zhanna_Gorelova@epam.com>
2018-01-09 19:57:42 +0300

    maven dependencies + servlet hello world

--- HelloServlet.java
+++ HelloServlet.java
@@ -1,5 +1,10 @@
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
-    //protected void doGet();
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().print("Hello World!");
+    }
 }
